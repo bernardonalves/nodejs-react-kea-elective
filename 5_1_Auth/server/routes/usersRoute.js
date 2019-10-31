b3772264d691797fc7dc93d0343c2e7f9b566eba
Bernardo Alves <bernardo@bellmetric.net>
2019-10-31 14:24:24 +0100

    Truncate instead of delete seeds, login/registration logic

--- 5_1_Auth/server/routes/usersRoute.js
+++ 5_1_Auth/server/routes/usersRoute.js
@@ -7,17 +7,6 @@ router.get('/users', async (req, res) => {
     res.json(users);
 });
 
-router.post('/users', async (req, res) => {
-    // TODO: Validate the input
-    console.log(req.body.form);
-    if(req.body.form.password){
-        const user = await User.query().insert(req.body.form);
-        res.json({ status: 200, response: "OK", user_created: user.username });
-    } else {
-        res.status(400).json({ response: "Bad Request"});
-    }
-});
-
 router.put('/users', async (req, res) => {
     res.send('Update User')
 });
@@ -26,9 +15,20 @@ router.delete('/users', async (req, res) => {
 });
 
 router.post('/users/login', async (req, res) => {
-    const user = await User.query().select().where({username: req.body.form.username});
+    const user = await User.query().select().where({username: req.body.form.username}).limit(1);
     if (user.length > 0) res.status(200).json({ response:"OK", username: user[0].username });
     else res.status(400).json({ response: "Bad Request"})
+});
+
+router.post('/users/register', async (req, res) => {
+    // TODO: Validate the input
+    console.log(req.body.form);
+    if(req.body.form.password){
+        const user = await User.query().insert(req.body.form);
+        res.json({ status: 200, response: "OK", user_created: user.username });
+    } else {
+        res.status(400).json({ response: "Bad Request"});
+    }
 });
 
 module.exports = router;
